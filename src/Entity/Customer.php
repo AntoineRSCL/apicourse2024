9b860e83479d20f1d58567829413982d802280d2
BoubouRSCL <abaut2001@gmail.com>
2024-01-21 13:21:48 +0100

    Last Push

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -16,7 +16,9 @@ use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\Link;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
 #[ApiResource(
@@ -30,37 +32,42 @@ use Symfony\Component\Serializer\Annotation\Groups;
         new Put(),
         new Delete(),
         new Patch()
-        
     ]
 )]
 #[ApiFilter(SearchFilter::class, properties:[
     "firstName" => "partial",
     "lastName",
     "company"
-] )]
+])]
 #[ApiFilter(OrderFilter::class)]
 class Customer
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['customers_read', 'invoices_read', 'users_read'])]
+    #[Groups(['customers_read','invoices_read','users_read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['customers_read', 'invoices_read', 'users_read'])]
+    #[Groups(['customers_read','invoices_read','users_read'])]
+    #[Assert\NotBlank(message: "Le prénom du customer est obligatoire")]
+    #[Assert\Length(min: 2, minMessage: "Le prénom doit faire entre 2 et 255 caractères", max: 255, maxMessage: "Le prénom doit faire entre 2 et 255 caractères maximum")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['customers_read', 'invoices_read', 'users_read'])]
+    #[Groups(['customers_read','invoices_read','users_read'])]
+    #[Assert\NotBlank(message: "Le nom du customer est obligatoire")]
+    #[Assert\Length(min: 2, minMessage: "Le nom doit faire entre 2 et 255 caractères", max: 255, maxMessage: "Le nom doit faire entre 2 et 255 caractères maximum")]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['customers_read', 'invoices_read', 'users_read'])]
+    #[Groups(['customers_read','invoices_read','users_read'])]
+    #[Assert\NotBlank(message:"L'adresse E-mail du customer est obligatoire")]
+    #[Assert\Email(message:"Le format de l'adresse E-mail doit être valide")]
     private ?string $email = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(['customers_read', 'invoices_read', 'users_read'])]
+    #[Groups(['customers_read','invoices_read','users_read'])]
     private ?string $company = null;
 
     #[ORM\OneToMany(mappedBy: 'customer', targetEntity: Invoice::class)]
@@ -70,6 +77,7 @@ class Customer
     #[ORM\ManyToOne(inversedBy: 'customers')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['customers_read'])]
+    #[Assert\NotBlank(message:"L'utilisateur est obligatoire")]
     private ?User $user = null;
 
     public function __construct()
@@ -78,31 +86,30 @@ class Customer
     }
 
     /**
-     * Permet de recup le total des invoices
+     * Permet de récup le total des invoices
      *
      * @return float
      */
-    #[Groups(['customers_read'])]
+    #[Groups('customers_read')]
     public function getTotalAmount(): float
     {
-        return round(array_reduce($this->invoices->toArray(), function($total, $invoice){
+        return round(array_reduce($this->invoices->toArray(),function($total,$invoice){
             return $total + $invoice->getAmount();
         },0),2);
     }
 
     /**
-     * Permet de recup le montant total non payé des factures
+     * Permet de récup le montant total non payé des factures
      *
      * @return float
      */
-    
-    #[Groups(['customers_read'])]
+    #[Groups('customers_read')]
     public function getUnpaidAmount(): float
     {
-        return round(array_reduce($this->invoices->toArray(), 
-        function($total, $invoice)
+        return round(array_reduce($this->invoices->toArray(),
+        function($total,$invoice)
         {
-            return $total + ($invoice->getAmount() === "PAID" || $invoice->getStatus() === "CANCELLED" ? 0 : $invoice->getAmount());
+            return $total + ($invoice->getStatus() === "PAID" || $invoice->getStatus() === "CANCELLED" ? 0 : $invoice->getAmount());
         },0),2);
     }
 
--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -16,7 +16,9 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\InvoiceRepository;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use App\Controller\InvoiceIncrementationController;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InvoiceRepository::class)]
 #[ApiResource(
@@ -28,22 +30,34 @@ use Symfony\Component\Serializer\Annotation\Groups;
     ],
     operations : [
         new Get(),
-        new GetCollection(),
         new Post(),
+        new Post(
+            controller:InvoiceIncrementationController::class,
+            uriTemplate: '/invoices/{id}/increment',
+            name: 'Increment',
+            openapiContext:[
+                'summary' => "Incrémente une facture",
+                'description' => "Incrémente le chrono d'une facture donnée"
+            ]
+        ),
+        new GetCollection(),
         new Put(),
         new Delete(),
         new Patch()
+    ],
+    denormalizationContext: [
+        "disable_type_enforcement" => true
     ]
 )]
 #[ApiResource(
     uriTemplate: '/customers/{id}/invoices',
     uriVariables: [
-        'id' => new Link(fromClass : Customer::class, fromProperty: 'invoices')
+        'id' => new Link(fromClass: Customer::class, fromProperty: 'invoices')
     ],
-    operations: [ new GetCollection() ], 
+    operations: [ new GetCollection() ],
     normalizationContext: [
         'groups' => ['invoices_subresource']
-    ]
+    ],
 )]
 #[ApiFilter(OrderFilter::class, properties:["amount","sentAt"])]
 class Invoice
@@ -51,36 +65,45 @@ class Invoice
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['invoices_read', 'customers_read', 'invoices_subresource'])]
+    #[Groups(['invoices_read','customers_read', 'invoices_subresource'])]
     private ?int $id = null;
 
     #[ORM\Column]
-    #[Groups(['invoices_read', 'customers_read', 'invoices_subresource'])]
-    private ?float $amount = null;
+    #[Groups(['invoices_read','customers_read', 'invoices_subresource'])]
+    #[Assert\NotBlank(message: "Le montant est obligatoire")]
+    #[Assert\Type(type:"numeric", message:"Le montant de la facture doit être au format numérique")]
+    private $amount = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups(['invoices_read', 'customers_read', 'invoices_subresource'])]
-    private ?\DateTimeInterface $sentAt = null;
+    #[Groups(['invoices_read','customers_read', 'invoices_subresource'])]
+    #[Assert\NotBlank(message: "La data de la facture est obligatoire")]
+    #[Assert\Type(type:"datetime", message:"La date doit être au format YYYY-MM-DD")]
+    private $sentAt = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['invoices_read', 'customers_read', 'invoices_subresource'])]
+    #[Assert\NotBlank(message: "Le statut de la facture est obligatoire")]
+    #[Assert\Choice(choices:["SENT","PAID","CANCELLED"],message:"Le statut doit être soit envoyé, payé ou annulé")]
+    #[Groups(['invoices_read','customers_read', 'invoices_subresource'])]
     private ?string $status = null;
 
     #[ORM\ManyToOne(inversedBy: 'invoices')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['invoices_read'])]
+    #[Assert\NotBlank(message: "Le client de la facture est obligatoire")]
     private ?Customer $customer = null;
 
     #[ORM\Column]
     #[Groups(['invoices_read', 'invoices_subresource'])]
-    private ?int $chrono = null;
+    #[Assert\NotBlank(message: "Le chrono de la facture est obligatoire")]
+    #[Assert\Type(type:"integer",message:"Le chrono de la facture doit être au format numérique")]
+    private $chrono = null;
 
     /**
-     * Permet de recuperer le user a qui appartient finalement la facture
+     * Permet de récup le user à qui appartient finalement la facture
      *
      * @return User
      */
-     #[Groups(['invoices_read', 'invoices_subresource'])]
+    #[Groups(['invoices_read','invoices_subresource'])]
     public function getUser(): User {
         return $this->customer->getUser();
     }
@@ -95,7 +118,7 @@ class Invoice
         return $this->amount;
     }
 
-    public function setAmount(float $amount): static
+    public function setAmount($amount): static
     {
         $this->amount = $amount;
 
@@ -107,7 +130,7 @@ class Invoice
         return $this->sentAt;
     }
 
-    public function setSentAt(\DateTimeInterface $sentAt): static
+    public function setSentAt($sentAt): static
     {
         $this->sentAt = $sentAt;
 
@@ -143,7 +166,7 @@ class Invoice
         return $this->chrono;
     }
 
-    public function setChrono(int $chrono): static
+    public function setChrono($chrono): static
     {
         $this->chrono = $chrono;
 
